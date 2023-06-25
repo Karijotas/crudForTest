c6116a8f114922a55eed30bb51675441d5797fec
Mantas Povilas Bagociunas <mpovilasb@live.com>
2023-06-25 17:57:21 +0300

    front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from "react-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DenseTable from './List';
+import ResponsiveAppBar from './ResponsiveAppBar';
+import FormPropsTextFields from './Create';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <HashRouter >
+      <Routes>
+      <Route path='/' element={<ResponsiveAppBar />} />
+      <Route path='/list' element={<DenseTable/>}/>
+      <Route path='/create' element={<FormPropsTextFields/>}/>
+
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
